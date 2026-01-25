67dbf30218f431627995e28c15e64278aea9e2f7
Nileshnhe <nehenilesh111@gmail.com>
2026-01-25 21:09:21 +0530

    first commit

---
+++ Day-02/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express()
+
+app.get("/", (req,res) => {
+    res.send("Express is working")
+})
+
+app.get("/home", (req,res) => {
+    res.send("This is home page")
+})
+
+app.listen("3000", () => {
+    console.log("Server is running on http://localhost:3000");
+    
+})
