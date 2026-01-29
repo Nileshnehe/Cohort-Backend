89a205289a781c5f87a07231ef4614cefa68ef8c
Nileshnhe <nehenilesh111@gmail.com>
2026-01-29 19:45:39 +0530

    Added day5 backend folder

---
+++ day-05/src/app.js
@@ -0,0 +1,18 @@
+
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+const notes = []
+
+app.post("/notes", (req, res) => {
+    
+    notes.push(req.body)
+
+    res.status(201).json({
+        message: "Note created sucessfully"
+    })
+})
+
+
+module.exports = app
