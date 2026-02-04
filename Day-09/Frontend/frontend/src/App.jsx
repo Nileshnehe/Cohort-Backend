a85adf28df93445a60574ea2245f6f2ea2edf854
Nileshnhe <nehenilesh111@gmail.com>
2026-02-04 13:13:57 +0530

    Implement frontend-backend API integration

--- Day-09/Frontend/frontend/src/App.jsx
+++ Day-09/Frontend/frontend/src/App.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
 
   const [notes, setNotes] = useState([
     {
-    title: "test title 1",
-    description: "test description 1" 
-  },
-   {
-    title: "test title 2",
-    description: "test description 2"
-  },
-   {
-    title: "test title 3",
-    description: "test description 3"
-  },
-   {
-    title: "test title 4",
-    description: "test description 4"
-  },
-])
+      title: "test title 1",
+      description: "test description 1"
+    },
+    {
+      title: "test title 2",
+      description: "test description 2"
+    },
+    {
+      title: "test title 3",
+      description: "test description 3"
+    },
+    {
+      title: "test title 4",
+      description: "test description 4"
+    },
+  ]);
+
+  axios.get("http://localhost:3000/api/notes")
+  .then((res) => {
+    setNotes(res.data.notes)
+  });
+
   return (
     <>
       <div className="notes">
