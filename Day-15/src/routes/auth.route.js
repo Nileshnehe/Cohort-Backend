5352440b474481d11c51797f62c63b9e2141a800
Nileshnhe <nehenilesh111@gmail.com>
2026-02-12 12:34:47 +0530

    insta clone initiate

--- Day-15/src/routes/auth.route.js
+++ Day-15/src/routes/auth.route.js
@@ -1,60 +1,12 @@
-const express  = require("express");
-const userModel = require('../models/user.model')
+const express = require("express");
+ const authController = require("../controller/auth.controller")
 const authRouter = express.Router()
-const crypto = require('crypto')
-const jwt = require('jsonwebtoken')
+
 
 // POST /api/auth/register
 
-authRouter.post("/register", async (req, res) => {
-       const { email, username, password, bio, profileImage } = req.body;
+authRouter.post("/register", authController.registerController)
 
-
-       const isUserAlreadyExists = await userModel.findOne({
-              $or: [
-                     { email },
-                     { username }
-              ]
-       })
-
-       if (isUserAlreadyExists) {
-              return res.status(409)
-                     .json({
-                            message: "user already exists" + (isUserAlreadyExists.email
-                                   == email ? "Email already exists" : "user name already exists")
-
-                     })
-       }
-
-       const hash = crypto.createHash('sha256').update(password).digest('hex')
-
-       const user = await userModel.create({
-              username,
-              email,
-              bio,
-              profileImage,
-              password: hash
-       })
-
-       // - user ka data hona chahiye
-       // - data uniq hona chahiye
-
-       const token = jwt.sign({
-              id: user._id,
-       }, process.env.JWT_SECRET,
-              { expiresIn: '1d' })
-
-              res.cookie("token", token)
-
-              res.status(201).json({
-                     message: "User registerd successfully",
-                     user: {
-                            email: user.email,
-                            username: user.username,
-                            bio: user.bio,
-                            profileImage: user.profileImage
-                     }
-              })
-})
+authRouter.post("/login", authController.loginController) 
 
 module.exports = authRouter
