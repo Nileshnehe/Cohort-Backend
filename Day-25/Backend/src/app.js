7b9daa67fd55563b7a3cc5c65bea99f4ac37b108
Nileshnhe <nehenilesh111@gmail.com>
2026-03-01 17:39:38 +0530

    feat(auth): add getMe endpoint with JWT-protected access

--- Day-25/Backend/src/app.js
+++ Day-25/Backend/src/app.js
@@ -6,5 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+/*
+** Routes
+*/
+const authRoutes = require("./routes/auth.route")
+
+app.use("/api/auth", authRoutes)
 
 module.exports = app
---
+++ Day-25/Backend/src/routes/auth.route.js
@@ -0,0 +1,12 @@
+const { Router } = require("express")
+const authController = require("../controller/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+
+const router = Router()
+
+router.post("/register", authController.registerUser)
+router.post("/login", authController.loginUser)
+
+router.get("/get-me",authMiddleware.authUser, authController.getMe)
+
+module.exports = router
