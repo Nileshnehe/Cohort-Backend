27052eb0ec3e635e9239dd834b7b2b2a8af7d77e
Nileshnhe <nehenilesh111@gmail.com>
2026-03-11 22:59:24 +0530

    feat(auth): implement user registration with validation and email notification

--- perplexity/src/app.js
+++ perplexity/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
-
+import authRouter from './routes/auth.route.js';
 
 const app = express();
 
@@ -8,11 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+ 
 
 // Health check
 app.get("/", (req, res) => {
     res.json({ message: "Server is running" });
 });
+
+app.use("/api/auth", authRouter);
 
 export default app;
---
+++ perplexity/src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        verified: {
+            type: Boolean,
+            default: false
+        },
+    },
+    { timestamps: true }
+);
+
+// Yeh ek **middleware** hai jo **automatically** chalta hai jab bhi user save hota hai DB mein.
+
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+// Yeh login ke time use hota hai:
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel
