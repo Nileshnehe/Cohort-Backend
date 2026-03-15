5dc8f6c0b849093e52822af77170e215068c3201
Nileshnhe <nehenilesh111@gmail.com>
2026-03-15 15:08:29 +0530

    feat(auth): implement Protected route component and enhance Login redirection

--- perplexity/Frontend/src/features/auth/pages/Login.jsx
+++ perplexity/Frontend/src/features/auth/pages/Login.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router'
 import { useAuth } from '../hook/useAuth'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router'
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     
+
+    const user = useSelector(state => state.auth.user);
+    const loading = useSelector(state => state.auth.loading) 
+
     const {handleLogin} = useAuth()
     const navigate = useNavigate();
 
@@ -24,6 +30,11 @@ const Login = () => {
 
 
     }
+
+    if(!loading && user){
+        return <Navigate to="/" replace/>
+    }
+
     return (
         <section className="min-h-screen bg-zinc-950 px-4 py-10 text-zinc-100 sm:px-6 lg:px-8">
             <div className="mx-auto flex min-h-[85vh] w-full max-w-5xl items-center justify-center">
