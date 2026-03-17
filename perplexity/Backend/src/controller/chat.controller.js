5440b0707c9945cec025b41254212a6f898afa8d
Nileshnhe <nehenilesh111@gmail.com>
2026-03-17 15:31:19 +0530

    feat: add chat functionality with AI response generation
    - Added @langchain/mistralai dependency for Mistral AI integration.
    - Implemented chat routes and controller to handle message sending.
    - Created AI service methods for generating responses and chat titles.
    - Updated app to include chat routes.
    - Defined chat model for storing user chats and messages.

---
+++ perplexity/Backend/src/controller/chat.controller.js
@@ -0,0 +1,39 @@
+import { generateResponse } from "../services/ai.service.js";
+import { generateChatTitle } from "../services/ai.service.js";
+import chatModel from "../models/chat.model.js";
+import messageModel  from "../models/message.model.js"
+
+
+export async function sendMessage(req, res) {
+
+    const { message } = req.body;
+
+    console.log(message);
+
+    const title = await generateChatTitle(message)
+    console.log(title)
+
+    const result = await generateResponse(message)
+
+    const chat = await chatModel.create({
+        user: req.user.id,
+        title
+    })
+
+    const aiMessage = await messageModel.create({
+        user: req.user._id,
+        chat: chat._id,  
+        content: result,
+        role: "ai"
+
+    })
+
+    res.status(200).json({
+        title,
+        chat,
+        aiMessage
+    })
+
+
+}
+
