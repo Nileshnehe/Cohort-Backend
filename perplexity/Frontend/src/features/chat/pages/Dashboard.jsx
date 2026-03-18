16de9ac6932212552ee5827128d834bcca992ceb
Nileshnhe <nehenilesh111@gmail.com>
2026-03-18 17:42:28 +0530

    feat(chat): add Chat and ChatWindow components with styling for chat interface

--- perplexity/Frontend/src/features/chat/pages/Dashboard.jsx
+++ perplexity/Frontend/src/features/chat/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import ChatWindow from '../components/ChatWindow'
 
 
 const Dashboard = () => {
 
   return (
-    <div className='min-h-screen bg-[#212121] text-[#ececec]'>
+    <div className='min-h-screen bg-[#212121] text-[#ececec] flex'>
       <Sidebar />
+      <ChatWindow />
     </div>
   )
 }
