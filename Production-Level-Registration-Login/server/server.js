7698b6c06dcde904a165f1b565078ee1d67fe5bd
Nileshnhe <nehenilesh111@gmail.com>
2026-05-28 17:16:52 +0530

    feat(server): initialize server with Express and MongoDB connection
    - Created package.json with necessary dependencies and scripts for development and production.
    - Implemented server.js to start the Express application and connect to the database.
    - Added app.js to set up Express middleware for JSON parsing.

---
+++ Production-Level-Registration-Login/server/server.js
@@ -0,0 +1,19 @@
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+
+
+const startServer = async () => {
+    try {
+        
+        await connectDB()
+
+        app.listen(3000, () => {
+    console.log(`server is running on port 3000`)
+});
+    } catch (error) {
+        console.log("Critical Startup Error:", error.message);
+        process.exit(1);
+    }
+}
+startServer()
