3d8d73d31d320418fb730089eb397f19a56df683
Nileshnhe <nehenilesh111@gmail.com>
2026-05-28 17:15:12 +0530

    feat(auth): add user model

---
+++ Production-Level-Registration-Login/server/src/models/user.model.js
@@ -0,0 +1,20 @@
+import { required } from "joi";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name:     { type: String, required: true, trim: true, },
+        email:    { type: String, required: true, unique: true, lowercase: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true }
+)
+
+userSchema.methods.toSafeObject = function () {
+  const { password, verificationToken, __v, ...safe } = this.toObject();
+  return safe;
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User
