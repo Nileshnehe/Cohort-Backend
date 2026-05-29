3369634ab48b1640afa45516755e9baaec6ca3d1
Nileshnhe <nehenilesh111@gmail.com>
2026-05-29 13:39:29 +0530

    feat(auth): create login validation schema

--- Production-Level-Registration-Login/server/src/validators/auth.validator.js
+++ Production-Level-Registration-Login/server/src/validators/auth.validator.js
@@ -8,10 +8,21 @@ export const registerSchema = Joi.object({
         .lowercase()
         .required(),
 
-    password:     Joi.string()
-    .min(6)
-    .pattern(/[A-Z]/,       'one uppercase letter')
-    .pattern(/[0-9]/,       'one number')
-    .pattern(/[!@#$%^&*]/, 'one special character')
-    .required(),
-})
+    password: Joi.string()
+        .min(6)
+        .pattern(/[A-Z]/, 'one uppercase letter')
+        .pattern(/[0-9]/, 'one number')
+        .pattern(/[!@#$%^&*]/, 'one special character')
+        .required(),
+});
+
+export const loginSchema = Joi.object({
+    email: Joi.string()
+        .email({ tlds: { allow: false } })
+        .lowercase()
+        .required(),
+
+        password: Joi.object()
+        .required()
+});
+
